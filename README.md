d66457ebaead448df8dfdfe6c19db764c81b83a2
MadelinTom <tom.madelin@gmail.com>
2021-11-17 14:55:42 +0000

    COS works

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Cloudant Migrate
 A tool to automate the migration of doc's from one cloudant instance to another
+
+It uses the [nodejs-cloudant](https://github.com/cloudant/nodejs-cloudant) SDK. For more information check out https://github.com/apache/couchdb-nano#database-functions.
+
+It appears nodejs-cloudant is old and there is a new version here: [cloudant-node-sdk](https://github.com/apache/couchdb-nano#database-functions)
+
+## Cloudant usage
+1. Create a .env file and fill in Cloudant credentials for both export and import instances
+2. run `source .env`
+3. run `npm run cloudant`
+
+## Cos usage
+1. Create a .env file and fill in COS credentials for both export and import instances
+2. run `source .env`
+3. run `npm run cos`
---
+++ cos/cos.js
@@ -0,0 +1,89 @@
+import ibm from "ibm-cos-sdk";
+
+// Globals
+let cosExportBucketNames = []; // holds bucket names of export instance
+let cosImportBucketNames = []; // holds bucket names of import instance (different as bucket names need to be globally unique)
+let cosExportBucketObjects = []; // holds objects from a bucket
+
+// Account details for both import and export COS instances
+const exportConfig = {
+  endpoint: process.env.COS_EXPORT_URL,
+  apiKeyId: process.env.COS_EXPORT_API_KEY,
+  serviceInstanceId: process.env.COS_EXPORT_RESOURCE_INSTANCE_ID,
+};
+const exportCos = new ibm.S3(exportConfig);
+
+const importConfig = {
+  endpoint: process.env.COS_IMPORT_URL,
+  apiKeyId: process.env.COS_IMPORT_API_KEY,
+  serviceInstanceId: process.env.COS_IMPORT_RESOURCE_INSTANCE_ID,
+};
+const importCos = new ibm.S3(importConfig);
+
+// Get all bucket names from export COS instance
+await exportCos
+  .listBuckets()
+  .promise()
+  .then((bucket) => {
+    cosExportBucketNames = bucket.Buckets;
+  });
+console.log(`--> Migrating ${cosExportBucketNames.length} buckets`);
+
+// Create all buckets in import COS instance
+const createBuckets = async () => {
+  for (let i = 0; i < cosExportBucketNames.length; i++) {
+    await importCos
+      .createBucket({
+        Bucket: `${cosExportBucketNames[i].Name}-copy`, // This is needed because bucket names need to be globally unique
+        CreateBucketConfiguration: {
+            LocationConstraint: "eu-de-standard"
+          },
+      })
+      .promise()
+      .catch((error) => console.log(error));
+  }
+};
+console.log(`--> Creating ${cosExportBucketNames.length} buckets`);
+await createBuckets();
+
+await importCos
+  .listBuckets()
+  .promise()
+  .then((bucket) => {
+    cosImportBucketNames = bucket.Buckets;
+  });
+
+// for each bucket, get all objects and upload to import bucket
+for (let i = 0; i < cosImportBucketNames.length; i++) {
+  let currentBucketName = cosExportBucketNames[i].Name;
+  console.log(`--> Begin migration of ${currentBucketName}`);
+
+  // List all objects from export instance and cache
+  await exportCos
+    .listObjects({ Bucket: `${currentBucketName}` })
+    .promise()
+    .then((obj) => {
+      cosExportBucketObjects = obj.Contents;
+    });
+  console.log(`--> Migrating ${cosExportBucketObjects.length} objects`);
+
+  // get full object from export instance and upload to import instance
+  for (let j = 0; j < cosExportBucketObjects.length; j++) {
+    const exportObject = await exportCos
+      .getObject({
+        Bucket: `${currentBucketName}`,
+        Key: `${cosExportBucketObjects[j].Key}`,
+      })
+      .promise();
+
+    await importCos
+      .putObject({
+        Bucket: `${currentBucketName}-copy`,
+        Key: `${cosExportBucketObjects[j].Key}`,
+        Body: exportObject.Body,
+      })
+      .promise();
+    console.log(`--> Object ${j}`);
+  }
+  console.log(`--> Done ${currentBucketName}`);
+}
